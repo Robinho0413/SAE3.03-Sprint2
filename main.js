fc3a70fcf8e08da6ade9ac6e112e1b8f666aebb5
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2023-12-20 17:44:55 +0100

    it2 bug fix

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ C.init = function () {
 
 }
 
+var chart2 = null
+
 M.CreateStackedBar = function (cm, td, tp) {
   let cmcopy = []
   let tdcopy = []
@@ -29,6 +31,8 @@ M.CreateStackedBar = function (cm, td, tp) {
   for(let elmt of tp){
     tpcopy.push(elmt)
   }
+
+  console.log(cmcopy)
   var options2 = {
     series: [{
       name: 'CM',
@@ -97,10 +101,13 @@ M.CreateStackedBar = function (cm, td, tp) {
     }
   };
 
-
-  var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+  if(chart2){
+    chart2.destroy();
+  }
+  chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
 
   chart2.render();
+
 
 }
 
@@ -220,6 +227,96 @@ var options3 = {
 };
 
 var chart = new ApexCharts(document.querySelector("#chart3"), options3);
+chart.render();
+
+
+
+//chart 4
+var options = {
+  series: [{
+    name: "Session Duration",
+    data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+  },
+  {
+    name: "Page Views",
+    data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+  },
+  {
+    name: 'Total Visits',
+    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+  }
+],
+  chart: {
+  height: 350,
+  type: 'line',
+  zoom: {
+    enabled: false
+  },
+},
+dataLabels: {
+  enabled: false
+},
+title: {
+  text: 'Page Statistics',
+  align: 'left'
+},
+legend: {
+  tooltipHoverFormatter: function(val, opts) {
+    return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+  }
+},
+markers: {
+  size: 0,
+  hover: {
+    sizeOffset: 6
+  }
+},
+xaxis: {
+  categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+    '10 Jan', '11 Jan', '12 Jan'
+  ],
+},
+tooltip: {
+  y: [
+    {
+      title: {
+        formatter: function (val) {
+          return val + " (mins)"
+        }
+      }
+    },
+    {
+      title: {
+        formatter: function (val) {
+          return val + " per session"
+        }
+      }
+    },
+    {
+      title: {
+        formatter: function (val) {
+          return val;
+        }
+      }
+    }
+  ]
+},
+grid: {
+  borderColor: '#f1f1f1',
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart4"), options);
 chart.render();
 
 
