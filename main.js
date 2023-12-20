aa4053bda3afa70e107956ded57d46b52b730905
Mathis Micheau <mathis.micheau@etu.unilim.fr>
2023-12-20 15:05:29 +0100

    IT2 erreur affichage

--- main.js
+++ main.js
@@ -6,45 +6,138 @@ let C = {};
 
 await M.init();
 
-C.init = function(){
+C.init = function () {
   V.init();
 
   //filtrer par groupe
   let group = document.querySelector('#select-groups');
   group.addEventListener('change', C.handler_changeOnGroup);
-  
+
 }
+
+M.CreateStackedBar = function (cm, td, tp) {
+  let cmcopy = []
+  let tdcopy = []
+  let tpcopy = []
+
+  for(let elmt of cm){
+    cmcopy.push(elmt)
+  }
+  for(let elmt of td){
+    tdcopy.push(elmt)
+  }
+  for(let elmt of tp){
+    tpcopy.push(elmt)
+  }
+  var options2 = {
+    series: [{
+      name: 'CM',
+      data: cmcopy,
+    }, {
+      name: 'TD',
+      data: tdcopy
+    }, {
+      name: 'TP',
+      data: tpcopy
+    },],
+    chart: {
+      type: 'bar',
+      height: 600,
+      stacked: true,
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        dataLabels: {
+          total: {
+            enabled: true,
+            offsetX: 0,
+            style: {
+              fontSize: '13px',
+              fontWeight: 900
+            }
+          }
+        }
+      },
+    },
+    stroke: {
+      width: 1,
+      colors: ['#fff']
+    },
+    title: {
+      text: 'Fiction Books Sales'
+    },
+    xaxis: {
+      categories: result,
+      labels: {
+        formatter: function (val) {
+          return val + " Hours"
+        }
+      }
+    },
+    yaxis: {
+      title: {
+        text: undefined
+      },
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + " Hours"
+        }
+      }
+    },
+    fill: {
+      opacity: 1
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'left',
+      offsetX: 40
+    }
+  };
+
+
+  var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+
+  chart2.render();
+
+}
+
+ 
+
+C.handler_changeOnGroup = function (ev) {
+  
+
+  if (ev.target.value == 'all') {
+    let data1 = M.getCountsByWeekWithCourse('CM', "0")
+    let data2 = M.getCountsByWeekWithCourse('TD', "0")
+    let data3 = M.getCountsByWeekWithCourse('TP', "0")
+
+    M.CreateStackedBar(data1, data2, data3)
+  }
+  else {
+
+    let data1 = M.getCountsByWeekWithCourse('CM', ev.target.value)
+    console.log(data1)
+    let data2 = M.getCountsByWeekWithCourse('TD', ev.target.value)
+    let data3 = M.getCountsByWeekWithCourse('TP', ev.target.value)
+
+    M.CreateStackedBar(data1, data2, data3)
+
+  }
+
+
+}
+
+
 
 let result = V.generateWeekNumbers()
 
-let courseEventsCM = M.getCountsByWeekWithCourse('CM');
 
-
-let courseEventsTD = M.getCountsByWeekWithCourse('TD');
-
-
-let courseEventsTP = M.getCountsByWeekWithCourse('TP');
 
 
 // affichage des groupes
-C.handler_changeOnGroup = function(ev){
-  let allEvents = M.getConcatEvents();
-
-  let eventsByGroup = [];
-
-  for(let event of allEvents){
-    if(event.groups.includes(ev.target.value)){
-      eventsByGroup.push(event);
-    }
-  }
-
-  //parcourir eventsByGroup
-
-
-  console.log(eventsByGroup);
-
-  return(eventsByGroup)
-}
 
 
 
@@ -54,7 +147,8 @@ C.handler_changeOnGroup = function(ev){
 
 
 
-// Chart volume d'heures de cours par semaine
+
+// Iteration 1 : Chart volume d'heures de cours par semaine
 
 var options = {
   chart: {
@@ -65,8 +159,8 @@ var options = {
     data: M.getCountsByWeek()
   }],
   xaxis: {
-    title : {
-      text : 'Numéros de semaines',
+    title: {
+      text: 'Numéros de semaines',
     },
     categories: result
   }
@@ -77,80 +171,56 @@ var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
 
 
-// Chart CM, TD, TP filtré par groupe de TP
+// Iteration 2 : Chart CM, TD, TP filtré par groupe de TP
 
-var options = {
+
+
+
+// Iteration 3 : Resssources et SAE
+
+var options3 = {
   series: [{
-  name: 'CM',
-  data: courseEventsCM,
-}, {
-  name: 'TD',
-  data: courseEventsTD
-}, {
-  name: 'TP',
-  data: courseEventsTP
-},],
+    name: 'S1 - Ressources',
+    data: [80, 50, 30, 40],
+  }, {
+    name: 'S1 - SAE',
+    data: [20, 30, 40, 90],
+  }, {
+    name: 'S2 - Ressources',
+    data: [44, 76, 78, 13],
+  }, {
+    name: 'S2 - SAE',
+    data: [64, 16, 38, 63],
+  }],
   chart: {
-  type: 'bar',
-  height: 600,
-  stacked: true,
-},
-plotOptions: {
-  bar: {
-    horizontal: true,
-    dataLabels: {
-      total: {
-        enabled: true,
-        offsetX: 0,
-        style: {
-          fontSize: '13px',
-          fontWeight: 900
-        }
-      }
+    height: 350,
+    type: 'radar',
+    dropShadow: {
+      enabled: true,
+      blur: 1,
+      left: 1,
+      top: 1
     }
   },
-},
-stroke: {
-  width: 1,
-  colors: ['#fff']
-},
-title: {
-  text: 'Fiction Books Sales'
-},
-xaxis: {
-  categories: result,
-  labels: {
-    formatter: function (val) {
-      return val + " Hours"
-    }
-  }
-},
-yaxis: {
   title: {
-    text: undefined
+    text: 'Radar Chart - Multi Series'
   },
-},
-tooltip: {
-  y: {
-    formatter: function (val) {
-      return val + " Hours"
-    }
+  stroke: {
+    width: 2
+  },
+  fill: {
+    opacity: 0.1
+  },
+  markers: {
+    size: 0
+  },
+  xaxis: {
+    categories: ['CM', 'TD', 'TP', 'Autres']
   }
-},
-fill: {
-  opacity: 1
-},
-legend: {
-  position: 'top',
-  horizontalAlign: 'left',
-  offsetX: 40
-}
 };
 
-var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
-chart2.render();
-
-
+var chart = new ApexCharts(document.querySelector("#chart3"), options3);
+chart.render();
 
 
 
